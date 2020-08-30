aa1fe12f84fbf6ad169e129c49e71e8f5a6a77d7
Tinky <48660658+Tinky124@users.noreply.github.com>
2020-08-30 21:20:09 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,18 +1,12 @@
 Config = {}
-
 -- # Locale to be used. You can create your own by simple copying the 'en' and translating the values.
 Config.Locale       				= 'en' -- Traduções disponives en / br
-
 -- # By how many services a player's community service gets extended if he tries to escape
 Config.ServiceExtensionOnEscape		= 8
-
 -- # Don't change this unless you know what you are doing.
 Config.ServiceLocation 				= {x =  170.43, y = -990.7, z = 30.09}
-
 -- # Don't change this unless you know what you are doing.
 Config.ReleaseLocation				= {x = 427.33, y = -979.51, z = 30.2}
-
-
 -- # Don't change this unless you know what you are doing.
 Config.ServiceLocations = {
 	{ type = "cleaning", coords = vector3(170.0, -1006.0, 29.34) },
@@ -29,8 +23,6 @@ Config.ServiceLocations = {
 	{ type = "gardening", coords = vector3(198.97, -1006.85, 29.29) },
 	{ type = "gardening", coords = vector3(201.47, -1004.37, 29.29) }
 }
-
-
 
 Config.Uniforms = {
 	prison_wear = {
